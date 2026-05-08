7f50957c20a89342b71157507723544189db9fb6
kenshoushouri-cloud <kenshou.shouri@gmail.com>
2026-05-08 18:47:47 +0900

    run_missing_results.py を更新

--- run_missing_results.py
+++ run_missing_results.py
@@ -344,7 +344,7 @@ def main():
     run_missing_check(
         start_date_str="2025-04-01",
         end_date_str="2026-04-30",
-        sleep_sec=0.7,
+        sleep_sec=0.5,
         record_no_race=True,
     )
 
