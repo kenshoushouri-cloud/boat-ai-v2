98754ba0076d377d1c09d68acd330f2e57fbfa0e
kenshoushouri-cloud <kenshou.shouri@gmail.com>
2026-06-25 16:34:54 +0900

    run_pre_day_test_fix1.py を作成

---
+++ run_pre_day_test_fix1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+import runpy
+from datetime import datetime, timedelta, timezone
+
+JST = timezone(timedelta(hours=9))
+os.environ.setdefault("TARGET_DATE", datetime.now(JST).strftime("%Y-%m-%d"))
+os.environ.setdefault("PRE_SESSION", "day")
+os.environ.setdefault("SELECTOR_MODE", "balanced")
+os.environ.setdefault("TEST_MODE", "1")
+os.environ.setdefault("DRY_RUN", "0")
+os.environ.setdefault("DAILY_LINE_LIMIT", "3")
+os.environ.setdefault("MONTHLY_LINE_LIMIT", "100")
+os.environ.setdefault("MAX_ITEMS_PER_MESSAGE", "6")
+os.environ.setdefault("MIN_ODDS_ROWS", "100")
+runpy.run_path("v24_pre_candidate_notifier_fix1.py", run_name="__main__")
