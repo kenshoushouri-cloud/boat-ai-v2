3add6ad41143d53b514913f0cf2c2261c5bdfd1c
kenshoushouri-cloud <kenshou.shouri@gmail.com>
2026-06-25 18:40:55 +0900

    run_final_test_fix1.py を作成

---
+++ run_final_test_fix1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+import runpy
+from datetime import datetime, timedelta, timezone
+
+JST = timezone(timedelta(hours=9))
+os.environ.setdefault("TARGET_DATE", datetime.now(JST).strftime("%Y-%m-%d"))
+os.environ.setdefault("SNAPSHOT_LABEL", "final")
+os.environ.setdefault("DECISION_LABEL", "final")
+os.environ.setdefault("SELECTOR_MODE", "balanced")
+os.environ.setdefault("REQUIRE_EXHIBITION", "0")
+os.environ.setdefault("TEST_MODE", "1")
+os.environ.setdefault("DRY_RUN", "0")
+os.environ.setdefault("DAILY_LINE_LIMIT", "3")
+os.environ.setdefault("MONTHLY_LINE_LIMIT", "100")
+os.environ.setdefault("MAX_ITEMS_PER_MESSAGE", "6")
+os.environ.setdefault("BATCH_NOTIFY", "1")
+runpy.run_path("v25_final_realtime_pipeline_fix1.py", run_name="__main__")
