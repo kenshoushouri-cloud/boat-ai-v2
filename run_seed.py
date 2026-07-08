6bb0ebde367327f6deef797f1bc4324bc78264e7
kenshoushouri-cloud <kenshou.shouri@gmail.com>
2026-07-08 21:34:44 +0900

    run_seed.py の削除

--- run_seed.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from app.jobs.race_seed_job import run_race_seed_job
-from utils.time_utils import today_str
-
-def main():
-    run_race_seed_job(today_str())
-
-if __name__ == "__main__":
-    main()
